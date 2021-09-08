42f0fe787280e5d8d9e32c3921df8ed223baad80
Zacharias-Zhao <zhaoshouxin1126@gmail.com>
2021-09-08 18:18:17 +0800

    rewrite

--- code/game/alone/hook_bk.js
+++ code/game/alone/hook_bk.js
@@ -3,11 +3,13 @@ function hook_encode() {
         var Base64 = Java.use("android.util.Base64");
         var ByteString = Java.use("com.android.okhttp.okio.ByteString");
         Base64.encodeToString.overload('[B', 'int').implementation = function (v1, v2) {
+            console.log("---------Base64 encodeToString start---------");
             console.log("v1:", ByteString.of(v1).utf8());
             console.log("v2:" + v2);
-            var encry = this.encodeToString(v1, v2);
-            console.log("encry:" + encry);
-            return encry
+            var result = this.encodeToString(v1, v2);
+            console.log("result:" + result);
+            console.log("---------Base64 encodeToString end---------");
+            return result
         };
     })
 }
@@ -17,10 +19,12 @@ function hook_sha1() {
     Java.perform(function x() {
         var DeviceUtil = Java.use('com.bk.base.util.bk.DeviceUtil');
         DeviceUtil.SHA1ToString.implementation = function (s1) {
+            console.log("---------DeviceUtil SHA1ToString start---------");
             console.log("s1:" + s1);
-            var st = this.SHA1ToString(s1);
-            console.log("st:" + st);
-            return st
+            var result = this.SHA1ToString(s1);
+            console.log("result:" + result);
+            console.log("---------DeviceUtil SHA1ToString end---------");
+            return result
         }
     })
 }
